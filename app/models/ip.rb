ad4535c099a5a6aa8a223740000c71fd7661491c
Перегарыч <andrew@bks.tv>
2016-11-28 16:17:44 +0300

    таки проверил ip на ip

--- app/models/ip.rb
+++ app/models/ip.rb
@@ -1,2 +1,10 @@
 class Ip < ApplicationRecord
+  @ip_regex = /^([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3}$/
+  validates :ip, 
+            :presence => true, 
+            :uniqueness => true,
+            :format => { :with => @ip_regex,:multiline => true } 
+
 end
+
+
