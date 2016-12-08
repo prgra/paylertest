2150b183ab0a69f30729c0b172b3be96532fb878
Перегарыч <andrew@bks.tv>
2016-12-08 09:44:46 +0300

    genips reindent using space 2

--- genips.rb
+++ genips.rb
@@ -5,11 +5,11 @@ uri = URI('http://77.232.128.205:3003/masscreate/')
 req = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json')
 ipsh=[]
 100.times do
-	ip = [rand(255),rand(255),rand(255),rand(255)].join(".")
-	ipsh.push({ip: ip})
+  ip = [rand(255),rand(255),rand(255),rand(255)].join(".")
+  ipsh.push({ip: ip})
 end
 
 req.body = ipsh.to_json
 res = Net::HTTP.start(uri.hostname, uri.port) do |http|
-	http.request(req)
+  http.request(req)
 end
